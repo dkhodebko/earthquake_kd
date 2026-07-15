590824fac1c69ce833251478b1d09c8b932f5ebd
dkodebko <dimakhodebko@gmail.com>
2026-07-15 21:31:19 +0300

    added dm1

---
+++ dags/fct_count_day_earthquake.py
@@ -0,0 +1,126 @@
+"""
+Построение витрины
+"""
+from airflow import DAG
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+import pendulum
+
+# Config
+OWNER = 'kd'
+DAG_ID = 'fct_count_day_earthquake'
+
+# Used tables in DAG
+LAYER = 'raw'
+SOURCE = 'earthquake'
+SCHEMA = 'dm'
+TARGET_TABLE = 'fct_count_day_earthquake'
+
+# PostgreSQL
+PG_CONNECT = 'dwh_postgres'
+
+DEFAULT_ARGS = {
+    'owner': OWNER,
+    'catchup': True,
+    'retries': 3,
+    'retry_delay': pendulum.duration(hours=1)
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    start_date=pendulum.datetime(2026, 6, 15),
+    schedule_interval="0 12 * * *",
+    tags=['dm', 'PostgreSQL'],
+    catchup=True,
+    concurrency=1,
+    max_active_tasks=1,
+    max_active_runs=1,
+    default_args=DEFAULT_ARGS,
+) as dag:
+
+    dag.doc_md = __doc__
+    
+    start = EmptyOperator(task_id='start')
+
+    sensor_on_raw_layer = ExternalTaskSensor(
+        task_id="sensor_on_raw_layer",
+        external_dag_id="raw_from_s3_to_pg",
+        allowed_states=["success"],
+        mode="reschedule",
+        timeout=360000,  # длительность работы сенсора
+        poke_interval=30,  # частота проверки
+    )
+
+    drop_stg_table_before = SQLExecuteQueryOperator(
+        task_id="drop_stg_table_before",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    create_stg_table = SQLExecuteQueryOperator(
+        task_id="create_stg_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        CREATE TABLE stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}" AS
+        SELECT
+            time::date AS date,
+            count(*)
+        FROM
+            ods.fct_earthquake
+        WHERE
+            time::date = '{{{{ data_interval_start.format('YYYY-MM-DD') }}}}'
+        GROUP BY 1
+        """,
+    )
+
+    drop_from_target_table = SQLExecuteQueryOperator(
+        task_id="drop_from_target_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DELETE FROM {SCHEMA}.{TARGET_TABLE}
+        WHERE date IN
+        (
+            SELECT date FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        )
+        """,
+    )
+
+    insert_into_target_table = SQLExecuteQueryOperator(
+        task_id="insert_into_target_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        INSERT INTO {SCHEMA}.{TARGET_TABLE}
+        SELECT * FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    drop_stg_table_after = SQLExecuteQueryOperator(
+        task_id="drop_stg_table_after",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    end = EmptyOperator(
+        task_id="end",
+    )
+
+    (
+            start >>
+            sensor_on_raw_layer >>
+            drop_stg_table_before >>
+            create_stg_table >>
+            drop_from_target_table >>
+            insert_into_target_table >>
+            drop_stg_table_after >>
+            end
+    )
